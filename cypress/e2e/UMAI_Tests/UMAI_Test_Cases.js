c2b4cadfd9187e28ab5975865ccc42c99da0d601
MohammedHendy94 <MohammedHendy94@gmail.com>
2022-10-23 16:38:22 +0200

    second test case

--- cypress/e2e/UMAI_Tests/UMAI_Test_Cases.js
+++ cypress/e2e/UMAI_Tests/UMAI_Test_Cases.js
@@ -4,13 +4,6 @@ describe('UMAI', ()=> {
 
     it('Validate user can book successfully', ()=> {
         cy.visit('/')
-
-      /*  cy.get('#um-reservation-date-picker').click()
-       cy.get('tbody').contains('tr').then( tableRow => {
-            cy.wrap(tableRow).find('.day-content').click()
-          })
-       */
-
           cy.get('#timeslot-football-time-0').click()
           //cy.contains('Sky Bar').click()
           cy.get('#um-field--first-name').type("Mohamed")
@@ -22,6 +15,29 @@ describe('UMAI', ()=> {
           cy.get('#ums-confirm-reservation-details-button').click()
          // cy.get('.um-dialog__button--confirm').click()
           cy.get('#ums-browse-more-button').should('contain','Make another reservation') 
+
+    })
+    it.only('Validate user cant enter invalid data or leave fields empty', ()=> {
+        cy.visit('/')
+          cy.get('#timeslot-football-time-0').click()
+          cy.contains('Sky Bar').click()
+          cy.get('#um-field--first-name').clear()
+          cy.get('#um-field--last-name').type("hendy")
+          cy.get('[name="phoneCountry"]').select('EG')
+          cy.get('#um-field--phone-number').type(" ")
+          cy.get('#um-field--email').type("@hendy")
+         // cy.contains('Phone number is required').should('contain','Phone number is required')
+          cy.get('[for="um-field--phone-number"]')
+          .parent('div')
+          .find('.um-form-error')
+          .should('contain','Phone number is required')
+
+          cy.contains('Please enter a valid email address').should('contain','Please enter a valid email address')
+
+
+          cy.get('#um-field--first-name').parent('div').find('.um-form-error').should('contain', 'First name is required')
+       
+      
 
     })
     
